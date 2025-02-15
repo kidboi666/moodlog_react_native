4d5422dd1709a4e16c5deb5fa3b26c6685b01271
예비 리액트맨 <70879978+kidboi666@users.noreply.github.com>
2025-02-15 15:55:36 +0900

    style : Pretendard 폰트 적용

--- tamagui.config.ts
+++ tamagui.config.ts
@@ -1,9 +1,48 @@
 import { defaultConfig } from '@tamagui/config/v4';
-import { createTamagui } from 'tamagui';
+import { createFont, createTamagui } from 'tamagui';
 import { themes, tokens } from '@tamagui/themes';
+
+const systemFont = createFont({
+  family: 'Pretendard',
+  size: {
+    1: 11,
+    2: 12,
+    3: 13,
+    4: 14,
+    true: 14,
+    5: 16,
+    6: 18,
+    7: 20,
+    8: 23,
+    9: 30,
+    10: 46,
+    11: 55,
+    12: 62,
+    13: 72,
+    14: 92,
+    15: 114,
+    16: 134,
+  },
+  weight: {
+    4: '400',
+    5: '500',
+    7: '700',
+    8: '800',
+  },
+  face: {
+    400: { normal: 'Pretendard-Regular' },
+    500: { normal: 'Pretendard-Medium' },
+    700: { normal: 'Pretendard-SemiBold' },
+    800: { normal: 'Pretendard-Bold' },
+  },
+});
 
 export const config = createTamagui({
   ...defaultConfig,
+  fonts: {
+    body: systemFont,
+    heading: systemFont,
+  },
   themes,
   tokens,
 });
