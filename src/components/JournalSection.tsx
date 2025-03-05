76b4f95f1325f76f665d1fa2fbb04b7924a764f7
예비 리액트맨 <70879978+kidboi666@users.noreply.github.com>
2025-03-05 14:16:31 +0900

    feat : JournalSection 컴포넌트 제거, GardenTitleHeader, GardenMonthUnits, GardenDayUnits, MoodBar, MoodSelectTitle, NextButton, EmptyExpandedContent, ProgressGraph, StatsContainer, SelectedMoodContainer, CollapsedContent 컴포넌트 추가

--- src/components/JournalSection.tsx
+++
@@ -1,8 +0,0 @@
-import { useJournal } from '@/store/hooks/useJournal';
-import { YStack } from 'tamagui';
-
-export const JournalSection = () => {
-  const { dailyJournals } = useJournal();
-
-  return <YStack></YStack>;
-};
