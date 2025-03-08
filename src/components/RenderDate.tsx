9fdc1ef954d74c0c2a5b1c2d793d9faa4c66ace7
예비 리액트맨 <70879978+kidboi666@users.noreply.github.com>
2025-03-08 11:04:47 +0900

    feat: RenderDate, RenderDay, RenderTime 컴포넌트 추가 및 기존 RenderDate, RenderTime 컴포넌트 삭제

---
+++ src/components/RenderDate.tsx
@@ -0,0 +1,60 @@
+import { Text, TextProps } from 'tamagui';
+import * as S from './RenderDate.styled';
+
+interface Props extends TextProps {
+  timestamp?: string | number;
+  localDate?: string;
+  onlyText?: boolean;
+}
+
+export const RenderDate = Text.styleable<Props>(
+  ({ timestamp, localDate, onlyText, ...props }, ref) => {
+    const renderText = (
+      year: string | number,
+      month: string | number,
+      day: string | number,
+    ) => {
+      return `${year}. ${month}. ${day}.`;
+    };
+
+    if (localDate) {
+      const [year, month, day] = localDate.split('-');
+
+      if (onlyText) return renderText(year, month, day);
+
+      return (
+        <S.Text ref={ref} {...props}>
+          {renderText(year, month, day)}
+        </S.Text>
+      );
+    }
+
+    if (timestamp) {
+      const date = new Date(timestamp);
+      const year = date.getFullYear();
+      const month = (date.getMonth() + 1).toString().padStart(2, '0');
+      const day = date.getDate().toString().padStart(2, '0');
+
+      if (onlyText) return renderText(year, month, day);
+
+      return (
+        <S.Text ref={ref} {...props}>
+          {renderText(year, month, day)}
+        </S.Text>
+      );
+    }
+
+    const today = new Date();
+    const year = today.getFullYear();
+    const month = (today.getMonth() + 1).toString().padStart(2, '0');
+    const day = today.getDate().toString().padStart(2, '0');
+
+    if (onlyText) return renderText(year, month, day);
+
+    return (
+      <S.Text ref={ref} {...props}>
+        {renderText(year, month, day)}
+      </S.Text>
+    );
+  },
+);
